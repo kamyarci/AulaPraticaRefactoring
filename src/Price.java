062902e1f0ce523845d7df48dddfdaafcd4ee0dc
Kamyla Arci <kamylaarci@gmail.com>
2025-11-13 17:53:08 -0300

    Commit 14

--- src/Price.java
+++ src/Price.java
@@ -2,6 +2,12 @@ package src;
 
 public abstract class Price {
     public abstract int getPriceCode();
-
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
